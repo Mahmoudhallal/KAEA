824bdc743d236be69b63c74c12a378e75b16c729
Mahmoud_h <mahmoud.hallal@dbmr.unibe.ch>
2018-12-19 14:17:40 +0100

    Delet some files

--- snakemake/stringi-master/devel/docker/docker_run_fedora_21.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run --rm -ti fedora:21 /bin/bash -c "
-yum upgrade -y -q                                                                       && \
-yum install -y -q libicu-devel R-devel git                                              && \
-`cat stringi_forcesystemicu.sh`                                                            \
-"
--- snakemake/stringi-master/devel/docker/docker_run_fedora_latest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# MG: added the `tar` rpm to prevent PDF man build failure
-
-docker run --rm -ti fedora:latest /bin/bash -c "
-dnf upgrade -y -q --refresh                                                             && \
-dnf install -y -q libicu-devel R-devel git tar                                          && \
-`cat stringi_forcesystemicu.sh`                                                            \
-"
